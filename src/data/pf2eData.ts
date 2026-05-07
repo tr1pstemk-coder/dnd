18f2f869ecdbfef8b4c93187aa7a1f99701a3039
tr1pstemk-coder <tr1ps.temk@gmail.com>
2026-05-08 01:22:35 +0200

    Add PF2e data: ancestries, classes, backgrounds, feats, equipment, heritages

---
+++ src/data/pf2eData.ts
@@ -0,0 +1,8 @@
+﻿export const ancestries = require('./pf2e/ancestries.json');
+export const classes = require('./pf2e/classes.json');
+export const backgrounds = require('./pf2e/backgrounds.json');
+export const feats = require('./pf2e/feats.json');
+export const equipment = require('./pf2e/equipment.json');
+export const heritages = require('./pf2e/heritages.json');
+export const classFeatures = require('./pf2e/class-features.json');
+export const ancestryFeatures = require('./pf2e/ancestry-features.json');
